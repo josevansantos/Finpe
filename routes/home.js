133205c23012cd6953cc35b01b91c725d7d62535
Josevan Santos <josevansnt@gmail.com>
2021-05-31 16:14:12 -0300

    aquivos

---
+++ routes/home.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Finpe' });
+});
+
+module.exports = router;
