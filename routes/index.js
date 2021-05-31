c1d6f7dbf97a6a832f16ebd7bdfe7b98b8f32263
Josevan Santos <josevansnt@gmail.com>
2021-05-31 14:57:52 -0300

    arrumando as rotas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Finpe' });
 });
+
+router.get('/login', function(req, res, next){
+  res.render('login')
+}); 
+
+router.get('/main', (req, res, next) => res.render('main'))
 
 module.exports = router;
