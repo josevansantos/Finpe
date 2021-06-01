a781b6f0b8e2ff4dc8496e6c926ece2785fac5fd
Josevan Santos <josevansnt@gmail.com>
2021-05-31 21:02:38 -0300

    fazendo configuracao do banco de dados

---
+++ settings/index.js
@@ -0,0 +1,12 @@
+const config = {
+  database: {
+    dialect:'mysql',
+    host:'localhost',
+    port:3306,
+    database:'finpe',
+    user:'root',
+    password:''
+  }
+}
+
+module.exports = config
