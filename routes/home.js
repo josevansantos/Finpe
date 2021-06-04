698f438c41aacbebfc2d9d7bb855615376bfdd35
Josevan Santos <josevansnt@gmail.com>
2021-06-03 23:35:17 -0300

    readme.md

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Finpe' });
-});
-
-module.exports = router;
--- src/transactions/readme.md
+++ src/transactions/readme.md
@@ -1,1 +1,3 @@
 Aqui vão ficar todos os arquivos relacionados as transações.
+
+transactios controller
