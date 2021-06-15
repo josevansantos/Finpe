1ee7ee665e9a88d657be4e464956587fa9a00937
Josevan Santos <josevansnt@gmail.com>
2021-06-15 17:34:09 -0300

    doc: criando arquivo de transaçoes

---
+++ database/models/Transactions.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const Transactions = sequelize.define('Transactions', {
+    description: DataTypes.STRING,
+    value: DataTypes.FLOAT,
+    type: DataTypes.STRING
+  },
+  {
+    tableName: "transactions",
+    timestamps: false,
+  }
+  );
+
+  Transactions.associate = models => {
+    Transactions.belongsTo(models.Users, {
+      as: 'user',
+      foreignKey: 'id_user'
+    })
+  }
+
+  return Transactions;
+}
