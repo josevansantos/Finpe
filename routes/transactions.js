dc25bda702c743234854e1c3328851a8d713e9d0
Josevan Santos <josevansnt@gmail.com>
2021-07-28 21:04:26 -0300

    criando rotas das transacoes

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const transaction = require('../controllers/transactions');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('transactions');
-});
+router.get('/', transaction.index);
+router.get('/', transaction.show);
+router.post('/', transaction.store);
+router.post('/', transaction.update);
+router.delete('/', transaction.destroy);
 
 module.exports = router;
