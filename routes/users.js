7e44f3968ed737856fc81e912533fd7e6bd825d5
Josevan Santos <josevansnt@gmail.com>
2021-07-28 21:27:16 -0300

    arruando as rotas

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const user = require('../controllers/users');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', user.index);
+router.get('/', user.show);
+router.post('/', user.store);
+router.post('/', user.update);
+router.delete('/', user.destroy);
 
 module.exports = router;
