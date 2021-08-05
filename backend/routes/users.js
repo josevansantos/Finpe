bacbfea7bb975bff74e1ae43347d25b8c6dfd064
Josevan Santos <josevansnt@gmail.com>
2021-08-05 16:31:26 -0300

    arruando rotas de usuarios

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const user = require('../controllers/Users');
 
-/* GET users listing. */
 router.get('/', user.index);
 router.get('/:id', user.show);
 router.post('/', user.store);
-router.post('/', user.update);
-router.delete('/', user.destroy);
+router.post('/:id', user.update);
+router.delete('/:id', user.destroy);
 
 module.exports = router;
